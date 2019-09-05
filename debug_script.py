0e926bbd07d34bb00c9fd7f4cd398a2e0f1d4b32
vankoj <51315090+vankoj@users.noreply.github.com>
2019-09-05 14:02:16 +0300

    Added base code for showing labels on hover
    Added base code for showing class names by hovering with the mouse on top of the drawn bounding boxes
    Added base code for predictions on an image

--- debug_script.py
+++ debug_script.py
@@ -1,17 +1,23 @@
 
 import os
+import sys
 
+
+assert len(sys.argv) >= 2, 'Wrong number of arguments! Required argument: input_file_path'
 
 CUR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-PYTHON_PATH = os.path.join(CUR_DIR_PATH, 'venv', 'Scripts', 'python.exe')
-INPUT_FILE_NAME = 'test.py'
-INPUT_FILE_PATH = os.path.join(CUR_DIR_PATH, INPUT_FILE_NAME)
+PYTHON_PATH = os.path.join(CUR_DIR_PATH, 'venv_Copy', 'Scripts', 'python.exe')
+INPUT_FILE_PATH = sys.argv[1]
+INPUT_FILE_NAME = INPUT_FILE_PATH.split('\\')[-1]
+TAB_SPACES_NUM = 4
+if len(sys.argv) >= 3:
+	TAB_SPACES_NUM = int(sys.argv[2])
 
 
 def tab_to_four_spaces(file_path):
     with open(file_path) as f:
         file_content = f.read()
-    file_content = file_content.replace('\t', '    ')
+    file_content = file_content.replace('\t', ' ' * TAB_SPACES_NUM)
     with open(file_path + '.tmp', 'w') as f:
 	    f.write(file_content)
 
